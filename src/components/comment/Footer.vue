bbe724d9463afd34e83ecef41297807482280511
ghostxbh <ghostxbh@hotmail.com>
2019-07-18 14:45:37 +0800

    修改底部样式

--- src/components/comment/Footer.vue
+++ src/components/comment/Footer.vue
@@ -2,7 +2,7 @@
   <div class="foot">
     <footer class="footer">
       <div class="container">
-        <p>Copyright &copy; 2019.ghostxbh All rights reserved.<a target="_blank" href="http://blog.uzykj.com/">http://blog.uzykj.com</a></p>
+        <p>Copyright &copy; 2019 ghostxbh All rights reserved <a target="_blank" href="http://blog.uzykj.com/">http://blog.uzykj.com</a></p>
       </div>
     </footer>
   </div>

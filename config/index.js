9d7a3f9410af0d9b3222bcb5d8d42e42d9c8f84b
ghostxbh <ghostxbh@hotmail.com>
2019-09-14 13:22:15 +0800

    modify service port

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api/':{
-        target:'http://127.0.0.1:8080/',
+        target:'http://127.0.0.1:8001/',
         changeOrigin:true,
         pathRewrite: {
           '^/api/':''
@@ -28,7 +28,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
